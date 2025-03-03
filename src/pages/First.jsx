f3f0f60afc853c7f8b56c3018b1b10752bcb107e
Aghakarim-dev <hemidzade1010@gmail.com>
2025-03-03 16:11:55 +0400

    redirect

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const First = () => {
   return (
-    <div className="h-screen text-white  bg-blue-600  flex flex-col gap-12 w-full items-center justify-center">
-      <h2 className="text-5xl tracking-widest">Başlıq</h2>
+    <div className="h-screen text-white  bg-black  flex flex-col gap-12 w-full items-center justify-center">
+      <h2 className="text-5xl tracking-widest">Başlıq <span className="bg-gradient-to-r from-orange-400 to-violet-500 text-transparent bg-clip-text">belə bəzəkli mətnlərdən ibarətdir</span></h2>
       <p className="p-12">
         Bu mətndə, müxtəlif mövzuların və ideyaların təsviri yer alır. Hər bir
         cümlə özündə müxtəlif mənalar daşıyır və ətraflı şəkildə izah edilir.
